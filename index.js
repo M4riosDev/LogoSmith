17c33c9ff117d2aa52662fc927c0b3356bda6232
fre3zog-arch <fre3zog@gmail.com>
2026-03-11 17:58:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
 });
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+});
+
+// όταν κάποιος μπαίνει στο server
+client.on('guildMemberAdd', member => {
+
+  // ID του role που θα παίρνει
+  const role = member.guild.roles.cache.get("1471461739026579588");
+
+  if (role) {
+    member.roles.add(role);
+  }
+
+  // channel για welcome message
+  const channel = member.guild.channels.cache.get("1450458984606863535");
+
+  if (channel) {
+    channel.send(`👋 Welcome ${member} to the server!`);
+  }
+
 });
 
 client.login(process.env.TOKEN);
